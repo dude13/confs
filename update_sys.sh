a70d35952536051f4a9228bc4a5a8cde9f5dec12
Douglas Reimberg de Oliveira <douglas.reimberg.oliveira@gmail.com>
2018-11-28 19:50:58 -0200

    Create update_sys.sh

---
+++ update_sys.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+echo
+echo
+echo system atualization ...
+echo
+sudo apt-get update 
+echo
+
+echo package atualization ...
+echo
+sudo apt-get upgrade -y 
+echo OKAY
+
+echo clean apt
+echo
+sleep 1
+sudo apt-get clean  
+echo OKAY
+
+echo autoremove apt
+echo
+sleep 1
+sudo apt autoremove 
+echo
+
+echo Update/Upgrade realized
+echo
+echo
+
