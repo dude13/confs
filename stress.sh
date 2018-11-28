40849a80761025f82543fcba64b9231da0a53621
Douglas Reimberg de Oliveira <douglas.reimberg.oliveira@gmail.com>
2018-11-28 20:43:29 -0200

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -1,14 +1,22 @@
 #/bin/bash
 
+
+echo
+echo
 echo Atualizando sistema
 
+echo
 sudo apt-get update && sudo apt-get upgrade -y
 
+echo
 echo Instalando stress-ng-test
 
+echo
 sudo apt-get install stress-ng -y
+echo
 
 echo Stress-full_test
+echo
 #Parametros
 #--cpu => faz um fork de processos a serem processados pela CPU
 #--cpu-method all => executa um teste de stress em todas as CPUs disponiveis
@@ -20,4 +28,9 @@ echo Stress-full_test
 #--hdd-bytes => tamanho da escrita para cada processo
 #--time-out => periodo do teste
 #--metrics-brief => mostra a saida das metricas apos execucao
-time stress-ng --cpu 2 --cpu-method all --cpu-ops 1000 --vm 2 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 120s --metrics-brief
+
+echo
+echo 
+echo >>>>> STRESS_NG TEST <<<<<<
+echo
+time stress-ng --cpu 4 --cpu-method all --cpu-ops 10000 --vm 6 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 240s --metrics-brief
