2950379a30849a3a28fc6932e265af7f99208cbe
Douglas Reimberg de Oliveira <douglas.reimberg.oliveira@gmail.com>
2019-01-04 10:34:51 -0200

    Update new-stress-ng.sh

--- new-stress-ng.sh
+++ new-stress-ng.sh
@@ -2,65 +2,81 @@
 
 clear
 sudo apt-get install htop xterm stress-ng -y
-
+#execute xterm htop |
 echo
 
 cat <<-EOF
-Please, choose your laptop model:
-(1) Dourado (Inspiron 14 / Inspiron 7460)
-(2) Emborrachado (Latitude 3470)
-(3) Aspero (Inspiron 15 /  Inspiron 5566)
-(4) Precision 5510 
-(5) Precision 5520 
-(6) Prata (Inspiron 15 / Inspiron 5448)
-(7) Latitude 5490 
+Abaixo segue os modelos dos laptops:
+
+	1 - Dourado (Inspiron 14 / Inspiron 7460)
+	2 - Emborrachado (Latitude 3470)
+	3 - Aspero (Inspiron 15 /  Inspiron 5566)
+	4 - Precision 5510 (ENG)
+	5 - Precision 5520 (ENG)
+	6 - Prata (Inspiron 15 / Inspiron 5448)
+	7 - Latitude 5490 
 EOF
-echo "Please talk to me"
+echo
+echo
+echo "Por favor, informe o numero do laptop a ser testado"
+echo
+echo
+
 while :
 do
   read INPUT_STRING
   case $INPUT_STRING in
   	1) 
 	   echo "Dourado (Inspiron 14 / Inspiron 7460)"
-	   time stress-ng --cpu 4 --cpu-method all --cpu-ops 10000 --vm 7 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 180s --metrics-brief
+	   echo
+	   echo "AGUARDE O TERMINO DO TESTE DE STRESS :PORFAVORZINHO:"
+       echo
+       time stress-ng --cpu 4 --cpu-method all --cpu-ops 10000 --vm 7 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 300s --metrics-brief
 	   break
 	   ;;
 	2) 
 	   echo "Emborrachado (Latitude 3470)"
-	   time stress-ng --cpu 4 --cpu-method all --cpu-ops 10000 --vm 7 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 180s --metrics-brief
+	   echo
+	   time stress-ng --cpu 4 --cpu-method all --cpu-ops 10000 --vm 7 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 300s --metrics-brief
 	   break
 	   ;;
 	3) 
 	   echo "Aspero (Inspiron 15 /  Inspiron 5566)"
-	   time stress-ng --cpu 4 --cpu-method all --cpu-ops 10000 --vm 7 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 180s --metrics-brief
+	   echo
+	   time stress-ng --cpu 4 --cpu-method all --cpu-ops 10000 --vm 7 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 300s --metrics-brief
 	   break
 	   ;;
 	4) 
 	   echo "Precision 5510" 
-	   time stress-ng --cpu 8 --cpu-method all --cpu-ops 10000 --vm 30 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 180s --metrics-brief
+	   echo
+	   time stress-ng --cpu 8 --cpu-method all --cpu-ops 10000 --vm 30 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 300s --metrics-brief
 	   break
 	   ;;
 	5) 
 	   echo "Precision 5520" 
-	   time stress-ng --cpu 8 --cpu-method all --cpu-ops 10000 --vm 30 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 180s --metrics-brief
+	   echo
+	   time stress-ng --cpu 8 --cpu-method all --cpu-ops 10000 --vm 30 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 300s --metrics-brief
 	   break
 	   ;;
 	6) 
 	   echo "Prata (Inspiron 15 / Inspiron 5448)"
-	   time stress-ng --cpu 4 --cpu-method all --cpu-ops 10000 --vm 7 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 180s --metrics-brief
+	   echo
+	   time stress-ng --cpu 4 --cpu-method all --cpu-ops 10000 --vm 7 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 300s --metrics-brief
 	   break
 	   ;;
 	7) 
 	   echo "Latitude 5490" 
-  	   time stress-ng --cpu 8 --cpu-method all --cpu-ops 10000 --vm 15 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 180s --metrics-brief
+	   echo 
+  	   time stress-ng --cpu 8 --cpu-method all --cpu-ops 10000 --vm 15 --vm-bytes 1G --io 1000 --hdd 50 --hdd-bytes 4G --timeout 300s --metrics-brief
 	   break
 	   ;;
 	*) 
 	   echo "Sorry, I don't understand"
+	   echo
 	   break
 	   ;;   
   esac
 done
 
-xterm htop&
+
 
