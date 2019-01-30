f9a45ff39215dc103a21baedfa3bd4ce2c38e3a4
Douglas Reimberg de Oliveira <douglas.reimberg.oliveira@gmail.com>
2019-01-30 16:29:09 -0200

    Update dmidecode.xpeer.sh

--- dmidecode.xpeer.sh
+++ dmidecode.xpeer.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-
-
-#!/bin/bash
-
-
 hostname
 hostname -I
 ls /home
 sudo dmidecode | grep -e  "BIOS Revision" -e "Product Name"
+
+sudo dmidecode -s system-product-name
+sudo dmidecode -s bios-version
+sudo dmidecode -s system-serial-number
+
 fdisk -l | grep "Disk /dev/sda"
 cat /etc/issue
-
-
 
